0b4e1734c289d1b16776119ff9b6b9d06fbeb71b
VIPIN-PASWAN <vipin1911016@akgec.ac.in>
2022-08-15 08:36:56 +0530

    Added 'SWIPER CDN' for smooth transition and its js code

--- js/script.js
+++ js/script.js
@@ -1,48 +1,119 @@
-window.onload = () => {
-	let button = document.querySelector("#btn");
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
 
-	// Function for calculating BMI
-	button.addEventListener("click", calculateBMI);
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
 };
 
-function calculateBMI() {
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
 
-	/* Getting input from user into height variable.
-	Input is string so typecasting is necessary. */
-	let height = parseInt(document
-			.querySelector("#height").value);
 
-	/* Getting input from user into weight variable.
-	Input is string so typecasting is necessary.*/
-	let weight = parseInt(document
-			.querySelector("#weight").value);
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
 
-	let result = document.querySelector("#result");
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        600:{
+            slidesPerView:2,
+        },
+    },
+});
 
-	// Checking the user providing a proper
-	// value or not
-	if (height === "" || isNaN(height))
-		result.innerHTML = "Provide a valid Height!";
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        991:{
+            slidesPerView:3,
+        },
+    },
+});
 
-	else if (weight === "" || isNaN(weight))
-		result.innerHTML = "Provide a valid Weight!";
 
-	// If both input is valid, calculate the bmi
-	else {
 
-		// Fixing upto 2 decimal places
-		let bmi = (weight / ((height * height)
-							/ 10000)).toFixed(2);
+// window.onload = () => {
+// 	let button = document.querySelector("#btn");
 
-		// Dividing as per the bmi conditions
-		if (bmi < 18.6) result.innerHTML =
-			`Under Weight : <span>${bmi}</span>`;
+// 	// Function for calculating BMI
+// 	button.addEventListener("click", calculateBMI);
+// };
 
-		else if (bmi >= 18.6 && bmi < 24.9)
-			result.innerHTML =
-				`Normal : <span>${bmi}</span>`;
+// function calculateBMI() {
 
-		else result.innerHTML =
-			`Over Weight : <span>${bmi}</span>`;
-	}
-}
+// 	/* Getting input from user into height variable.
+// 	Input is string so typecasting is necessary. */
+// 	let height = parseInt(document
+// 			.querySelector("#height").value);
+
+// 	/* Getting input from user into weight variable.
+// 	Input is string so typecasting is necessary.*/
+// 	let weight = parseInt(document
+// 			.querySelector("#weight").value);
+
+// 	let result = document.querySelector("#result");
+
+// 	// Checking the user providing a proper
+// 	// value or not
+// 	if (height === "" || isNaN(height))
+// 		result.innerHTML = "Provide a valid Height!";
+
+// 	else if (weight === "" || isNaN(weight))
+// 		result.innerHTML = "Provide a valid Weight!";
+
+// 	// If both input is valid, calculate the bmi
+// 	else {
+
+// 		// Fixing upto 2 decimal places
+// 		let bmi = (weight / ((height * height)
+// 							/ 10000)).toFixed(2);
+
+// 		// Dividing as per the bmi conditions
+// 		if (bmi < 18.6) result.innerHTML =
+// 			`Under Weight : <span>${bmi}</span>`;
+
+// 		else if (bmi >= 18.6 && bmi < 24.9)
+// 			result.innerHTML =
+// 				`Normal : <span>${bmi}</span>`;
+
+// 		else result.innerHTML =
+// 			`Over Weight : <span>${bmi}</span>`;
+// 	}
+// }
